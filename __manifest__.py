3b3896dcb7a23ecc0bd131dc48130f61873ac7d7
Gian Gianna Pallan Pallas <gian.ule@gmail.com>
2019-10-14 23:04:07 +0700

    academic v10

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+   "name": "Academic Addons Path Custom",
+   "version": "1.0",
+   "depends": [
+      "base",
+   ],
+   "author": "gian.ule@gmail.com",
+   "category": "Lathan Odoo",
+   'website': 'http://www.kodequ.com',
+   "description": """\
+        Buat Modul Academic Path Custom
+    """,
+   "data": [
+      "menu.xml",
+      "course.xml",
+      "session.xml",
+      "attendee.xml",
+      "partner.xml"
+   ],
+   "installable": True,
+   "auto_install": False,
+   "application": True,
+}
